091a861829dabcf9081e1e5aa5a1b79ffe817a1c
Jun Kojima <luxeno103@gmail.com>
2023-05-30 17:16:37 +0900

    pages navgator

--- lib/dm_page.dart
+++ lib/dm_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class DmPage extends StatelessWidget{
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 
 }
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget{
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 
 }
